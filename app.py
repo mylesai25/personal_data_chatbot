7646e9cc743d826949a517e7d14421fef5910db1
Myles Ingram <143450175+mylesai25@users.noreply.github.com>
2024-07-07 15:56:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -275,7 +275,7 @@ if st.session_state.uploaded_file and os.environ['OPENAI_API_KEY']:
             with st.expander("Sources"):
                 for i, node in enumerate(st.session_state.source_nodes):
                     st.write(f"""
-                                **{i+1}. Text:** {node.replace("*","")} 
+                                **{i+1}. Text:** {node.metadata['original_text'].replace("*","")} 
                                 """)
         st.session_state.messages.append({'role': 'assistant', 'content': response})
 

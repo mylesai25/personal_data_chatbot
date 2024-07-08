498881a2274ce93d98f3db7ed28477a0456f4485
Myles Ingram <143450175+mylesai25@users.noreply.github.com>
2024-07-07 23:47:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -251,10 +251,10 @@ if st.session_state.api_type:
         os.environ['OPENAI_API_KEY'] = st.sidebar.text_input('OpenAI API Key', type='password')
     elif st.session_state.api_type == 'Anthropic':
         st.session_state.model_name = st.sidebar.selectbox('Which model would you like to use?', anthropic_models)
-        os.environ['OPENAI_API_KEY'] = st.sidebar.text_input('Anthropic API Key', type='password')
+        os.environ['ANTHROPIC_API_KEY'] = st.sidebar.text_input('Anthropic API Key', type='password')
     elif st.session_state.api_type == 'Anyscale':
         st.session_state.model_name = st.sidebar.selectbox('Which model would you like to use?', anyscale_models)
-        os.environ['OPENAI_API_KEY'] = st.sidebar.text_input('Anyscale API Key', type='password')
+        os.environ['ANYSCALE_API_KEY'] = st.sidebar.text_input('Anyscale API Key', type='password')
 
 st.session_state.uploaded_file = st.sidebar.file_uploader("Upload document", type=['docx', 'pdf'], accept_multiple_files=False)
 
